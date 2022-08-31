14b279e46cfaf9055860c227a74abd041b48f30d
Alex Silva <alex.silva@avioconsulting.com>
2022-08-31 09:33:11 -0300

    basics intro start

---
+++ 1_INTRO/hello-world/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      basico do react
+      hello world.
+    </div>
+  );
+}
+
+export default App;
